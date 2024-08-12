c3ee4cc979e249cadac8eb40f3ea1d66d5b32a8e
David Suescun Caro <dsuescun@soinsoluciones.com>
2024-08-12 18:47:01 -0500

    Se ajusta para soportar el modelo dall-e-3

--- lib/generate-images.ts
+++ lib/generate-images.ts
@@ -41,15 +41,16 @@ const generatePrompt = async (colorList: string[], escudoImage: string): Promise
 
     console.log("escudoDescription", escudoDescription);
 
+    const imageModel = process.env.MODEL_GEN_IMAGE || 'dall-e-2';
     const { text } = await generateText({
         model: google('models/gemini-1.5-pro-latest'),
-        prompt: `Construye un ejemplo de prompt específico para generar la imagen de un uniforme de futbol con dall-e teniendo en cuenta lo siguiente:
+        prompt: `Construye un ejemplo de prompt específico para generar la imagen de un uniforme de futbol con ${imageModel} teniendo en cuenta lo siguiente:
 - La imagen debe ser realista
 - Debe tener camiseta, medias y pantaloneta
 - Los colores representativos del uniforme deben ser: ${colorList.join(", ")}
 - Debe incluir pequeños detalles del escudo en la parte superior de la camiseta
 - El escudo es así: ${escudoDescription}.
-El ejemplo del prompt debe estar listo para copiar y pegar en la plataforma de dall-e.`,
+El ejemplo del prompt debe estar listo para copiar y pegar en la plataforma de ${imageModel}.`,
     });
 
     return text;
@@ -57,18 +58,29 @@ El ejemplo del prompt debe estar listo para copiar y pegar en la plataforma de d
 
 const generateImages = async (promptText: string): Promise<string[]> => {
     const openai = new OpenAI();
-
-    const response = await openai.images.generate({
-        model: process.env.MODEL_GEN_IMAGE || 'dall-e-2',
+    const imageModel = process.env.MODEL_GEN_IMAGE || 'dall-e-2';
+    const size = '1024x1024';
+    const n = 1;
+    
+    // Se generan las imagenes una por una ya que dall-e-3 no soporta n > 1
+    const response1 = await openai.images.generate({
+        model: imageModel,
         prompt: promptText,
-        size: '1024x1024',
-        n: 2,
+        size: size,
+        n: n,
     });
+    console.log("image1", response1.data[0].url || "");
 
-    console.log("images", response.data.join(", "));
+    const response2 = await openai.images.generate({
+        model: imageModel,
+        prompt: promptText,
+        size: size,
+        n: n,
+    });
+    console.log("image2", response2.data[0].url || "");
 
     return [
-        response.data[0].url || "",
-        response.data[1].url || "",
+        response1.data[0].url || "",
+        response2.data[0].url || "",
     ];
 }
